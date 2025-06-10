cbabd18d1f0ac0a49daa05bbe0c9a0116ab7b872
pyschonoice <monukk2002@gmail.com>
2025-06-11 00:04:15 +0530

    work now pls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      host: '172.25.154.96',
-      protocol: 'ws',
-    },
+    // host: '0.0.0.0',
+    // port: 5173,
+    // strictPort: true,
+    // hmr: {
+    //   host: '172.25.154.96',
+    //   protocol: 'ws',
+    // },
     proxy: {
-      '/': {
+      '/api': {
         target: process.env.VITE_APP_API_BASE,
         changeOrigin: true
       }
